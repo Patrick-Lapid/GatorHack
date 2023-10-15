7d0736a719d6374384d95b818dcf10e7d7af966e
Patrick <patrickklapid@gmail.com>
2023-10-15 04:00:52 -0400

    cleaned up file structure

--- client/src/Overlay.tsx
+++ client/src/Overlay.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './contentscript.css';
 import { motion as m } from 'framer-motion';
 import useMeasure from "react-use-measure";
 import { SquareMinus } from 'tabler-icons-react';
 import AudioRecorder from './components/AudioRecorder';
 import MicRecorder from 'mic-recorder-to-mp3';
-import ReactDOM from 'react-dom';
+import { filterPhoneSection } from './scripts/filter';
 
 const registry = new Map<string,(arg0: any) => void>();
 
@@ -60,94 +60,23 @@ const callBack = (buffer : ArrayBuffer, blob : Blob) => {
     
   };
 
-const filterCategories = []
 
-const filter = async (filterParams: any) => {
-
-    console.log("filtering...");
-    const brandDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(1) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const osDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(2) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const specialOffersDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(3) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const inStorePickupDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(4) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const priceDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(5) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const conditionDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(6) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const availabilityDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(7) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    const colorDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(8) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
-    
-    brandDropdown && brandDropdown.click();
-    osDropdown && osDropdown.click();
-    specialOffersDropdown && specialOffersDropdown.click();
-    inStorePickupDropdown && inStorePickupDropdown.click();
-    priceDropdown && priceDropdown.click();
-    conditionDropdown && conditionDropdown.click();
-    availabilityDropdown && availabilityDropdown.click();
-    await colorDropdown && colorDropdown.click();
-
-    function getCheckboxByValue(val : string) {
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(function(checkbox : any) {
-            if (checkbox.attributes.value.value === val) {
-                checkbox.click();
-            }
-        });
-    }
-
-    for(const val of filterParams){
-        await getCheckboxByValue(val);
-    }
-
-    await getCheckboxByValue("Nokia");
-    await getCheckboxByValue("Google");
-
-    setTimeout(() => {
-        const apply : HTMLElement | null = document.querySelectorAll('[data-testid="applyFilter"]') as any
-        apply && apply.click();
-      }, 1000);
-  
-    
-    // applyFilter
-    
-    // const brandBoxes : HTMLElement = document.getElementById('#clnr2qe00005q359mr9ec2jj8') as HTMLElement;
-    
-    // brandBoxes.click();
-
-    // for(const val of filterParams){
-    //     if(val.length === 0){
-    //         continue;
-    //     }
-
-    //     for(const item of val){
-
-    //     }
-    // }
+const characterAnimation = {
+    hidden: {
+      opacity: 0,
+      y: `0.25em`,
+    },
+    visible: {
+      opacity: 1,
+      y: `0em`,
+      transition: {
+        duration: 1,
+        ease: [0.2, 0.65, 0.3, 0.9],
+      },
+    },
+  };
 
 
-    
-    // if ($('body:contains("Filter")').length) {
-    //     for(const [key, param] of filterParams.entries()){
-    //         if(!param){
-    //             continue;
-    //         } else {
-    //             console.log(key, param);
-    //             // const element = document.querySelector(`[data-testid="${param}"]`);
-    //             const element : HTMLElement = document.querySelector('#accordian-content > div > div > div:nth-child(1)') as HTMLElement;
-                
-    //             if (element) {
-    //                 element.click();
-    //                 console.log('Element found:', element);
-    //             } else {
-    //                 console.log('Element not found.');
-    //             }
-                  
-                               
-    //         }
-    //     }
-    //     // filterParams.map((x : any) => console.log(x));
-    // } else {
-    //     console.log("No Filter on Page.");
-    // }
-
-}
 
 const Overlay = () => {
     const chatRef = React.useRef<HTMLInputElement>(null);
@@ -169,8 +98,8 @@ const Overlay = () => {
     )
 
     registerType("filter", async (arr: any) => {
-        // filterParam = {}
-        filter(arr);
+        const filterParams = ["Google", "Nokia", "$30-$40"]
+        filterPhoneSection(filterParams);
     })
 
     return (        
@@ -230,7 +159,7 @@ const Overlay = () => {
                         callback={callBack}
                     />
                     <input type='text' id="chat" ref={chatRef} className="block active:shadow-md mx-4 p-2.5 w-full text-sm rounded-lg border bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Your message..." />
-                        <button onClick={(event) => {event.preventDefault(); handleMessageSend() }} className="inline-flex border-none justify-center p-2 rounded-full cursor-pointer text-blue-500 hover:bg-gray-600">
+                        <button onClick={(event) => {event.preventDefault(); setExpanded(true); handleMessageSend() }} className="inline-flex border-none justify-center p-2 rounded-full cursor-pointer text-blue-500 hover:bg-gray-600">
                         <svg className="w-5 h-5 border-none rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                             <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
                         </svg>
