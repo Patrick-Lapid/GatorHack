4996dc31e20bf8ceb0f32cccc6543c104d7ebce7
Patrick <patrickklapid@gmail.com>
2023-10-15 03:36:07 -0400

    filter logic

--- client/src/Overlay.tsx
+++ client/src/Overlay.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './contentscript.css';
 import { motion as m } from 'framer-motion';
 import useMeasure from "react-use-measure";
 import { SquareMinus } from 'tabler-icons-react';
 import AudioRecorder from './components/AudioRecorder';
 import MicRecorder from 'mic-recorder-to-mp3';
+import ReactDOM from 'react-dom';
 
 const registry = new Map<string,(arg0: any) => void>();
 
@@ -17,7 +18,7 @@ const ws = new WebSocket('ws://localhost:8765');
 
     // Log any messages received from the server
     ws.onmessage = (message) => {
-        console.log(`Received: ${message.data}`);
+        // console.log(`Received: ${message.data}`);
         const data = JSON.parse(message.data)
         
         const lambda = registry.get(data.type)
@@ -29,7 +30,8 @@ const ws = new WebSocket('ws://localhost:8765');
 
     // Log any errors
     ws.onerror = (error) => {
-        console.log(`WebSocket error: ${error}`);
+        console.log(`WebSocket error: ${JSON.stringify(error)}`);
+        
     };
 
 
@@ -38,7 +40,6 @@ const ws = new WebSocket('ws://localhost:8765');
     }
 
 const callBack = (buffer : ArrayBuffer, blob : Blob) => {
-    
     
     ws.send(buffer)
     //no clue why this won't compile without "buffer as any"
@@ -52,9 +53,96 @@ const callBack = (buffer : ArrayBuffer, blob : Blob) => {
     
   };
 
-const Overlay = () => {
+const filterCategories = []
 
-    console.log("y u no load")
+const filter = async (filterParams: any) => {
+
+    console.log("filtering...");
+    const brandDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(1) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const osDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(2) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const specialOffersDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(3) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const inStorePickupDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(4) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const priceDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(5) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const conditionDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(6) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const availabilityDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(7) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    const colorDropdown : HTMLElement | null = document.querySelector('#accordian-content > div > div > div:nth-child(8) > div.StyledAccordionItem-VDS__sc-h2bqgc-1.mCOTK > div.WrapperDiv-VDS__sc-19osi3m-3.jFGcii > button') as HTMLElement;
+    
+    brandDropdown && brandDropdown.click();
+    osDropdown && osDropdown.click();
+    specialOffersDropdown && specialOffersDropdown.click();
+    inStorePickupDropdown && inStorePickupDropdown.click();
+    priceDropdown && priceDropdown.click();
+    conditionDropdown && conditionDropdown.click();
+    availabilityDropdown && availabilityDropdown.click();
+    await colorDropdown && colorDropdown.click();
+
+    function getCheckboxByValue(val : string) {
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(function(checkbox : any) {
+            if (checkbox.attributes.value.value === val) {
+                checkbox.click();
+            }
+        });
+    }
+
+    for(const val of filterParams){
+        await getCheckboxByValue(val);
+    }
+
+    await getCheckboxByValue("Nokia");
+    await getCheckboxByValue("Google");
+
+    setTimeout(() => {
+        const apply : HTMLElement | null = document.querySelectorAll('[data-testid="applyFilter"]') as any
+        apply && apply.click();
+      }, 1000);
+  
+    
+    // applyFilter
+    
+    // const brandBoxes : HTMLElement = document.getElementById('#clnr2qe00005q359mr9ec2jj8') as HTMLElement;
+    
+    // brandBoxes.click();
+
+    // for(const val of filterParams){
+    //     if(val.length === 0){
+    //         continue;
+    //     }
+
+    //     for(const item of val){
+
+    //     }
+    // }
+
+
+    
+    // if ($('body:contains("Filter")').length) {
+    //     for(const [key, param] of filterParams.entries()){
+    //         if(!param){
+    //             continue;
+    //         } else {
+    //             console.log(key, param);
+    //             // const element = document.querySelector(`[data-testid="${param}"]`);
+    //             const element : HTMLElement = document.querySelector('#accordian-content > div > div > div:nth-child(1)') as HTMLElement;
+                
+    //             if (element) {
+    //                 element.click();
+    //                 console.log('Element found:', element);
+    //             } else {
+    //                 console.log('Element not found.');
+    //             }
+                  
+                               
+    //         }
+    //     }
+    //     // filterParams.map((x : any) => console.log(x));
+    // } else {
+    //     console.log("No Filter on Page.");
+    // }
+
+}
+
+const Overlay = () => {
 
     const image1Url = chrome.runtime.getURL('images/mic_black.png');
     const [expanded, setExpanded] = useState(false);
@@ -65,6 +153,11 @@ const Overlay = () => {
         const str = s as string
         setResponse(response+str)}
     )
+
+    registerType("filter", async (arr: any) => {
+        // filterParam = {}
+        filter(arr);
+    })
 
     return (        
 
@@ -111,8 +204,8 @@ const Overlay = () => {
                 <div className="flex items-center px-3 py-2 rounded-lg bg-gray-700">
                     
                     <AudioRecorder
-                        width="20px"
-                        height="20px"
+                        width="30px"
+                        height="30px"
                         bgcolor="#138481"
                         black={false}
                         recorder={
